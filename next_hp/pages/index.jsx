ae300633d29254efc371bc5e83c7779e3d2d8f25
okamoto <okamoto_yu-ki@outlook.jp>
2022-01-23 19:06:41 +0900

    first commit

---
+++ next_hp/pages/index.jsx
@@ -0,0 +1,11 @@
+import { Layout } from "../components/Layout";
+
+const Home = () => {
+  return (
+    <Layout title="Home">
+      <p className="text-4xl">Hello Nextjs</p>
+    </Layout>
+  );
+};
+
+export default Home;
